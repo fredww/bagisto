18f5b954c8ec42b8c062573ec91afc738b700836
fredww <fred.wang@silksoftware.com>
2025-11-24 22:16:09 +0800

    fix(ThemeViewFinder): 修复未设置前台主题的问题
    当$isAdmin为false时未设置前台默认主题，现补充else分支设置shop-default主题

--- packages/Webkul/Theme/src/ThemeViewFinder.php
+++ packages/Webkul/Theme/src/ThemeViewFinder.php
@@ -55,6 +55,8 @@ class ThemeViewFinder extends FileViewFinder
     {
         if ($isAdmin) {
             themes()->set(config('themes.admin-default'));
+        } else {
+            themes()->set(config('themes.shop-default'));
         }
     }
 
