31de9aa1a376fe5d339bec155873ee1ac30f25ff
fredww <fred.wang@silksoftware.com>
2025-12-14 11:20:18 +0800

    test

--- packages/Webkul/Airwallex/src/Http/Controllers/AirwallexController.php
+++ packages/Webkul/Airwallex/src/Http/Controllers/AirwallexController.php
@@ -87,6 +87,8 @@ class AirwallexController extends Controller
             }
         }
 
+        Log::warning('test:'.printf('%s %s %s', $signingValue, $signature, $sharedSecret));
+        Log::warning('body:'.printf('%s %s %s', $body));
         if (! $signingValue || ! $signature || ! $sharedSecret || ! $this->service->verifyWebhookSignature($signingValue, $signature, $sharedSecret, $body)) {
             Log::warning('Airwallex webhook signature invalid');
 
